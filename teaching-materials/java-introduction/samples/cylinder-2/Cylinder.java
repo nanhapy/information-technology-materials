480ae0731678731179c3425bf13f0860684e4f0f
zhaolei <pzl1988p@gmail.com>
2019-10-02 00:14:00 +0800

    add java intro

---
+++ teaching-materials/java-introduction/samples/cylinder-2/Cylinder.java
@@ -0,0 +1,27 @@
+public class Cylinder {
+
+    private Circle circle;
+
+    public Circle getCircle() {
+        return circle;
+    }
+
+    public void setCircle(Circle circle) {
+        this.circle = circle;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return this.height * this.circle.getArea();
+    }
+
+    private double height;
+
+}
