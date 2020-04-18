d5a2b9718e6f19d367688a551d9f6fb42d4b5f77
zhaolei <pzl1988p@gmail.com>
2020-04-18 22:22:54 +0800

    update

---
+++ teaching-materials/java-introduction/samples/Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 12;
+        int f1 = 1;
+        int f2 = 1;// Fibonacci第一个和第二个数为1
+        int f3;// 接收新的Fibonacci数
+
+        for (int i = 3; i <= n; i++) {// 输出多少个看自己需要
+            f3 = f1 + f2;// 新的Fibonacci数
+            f1 = f2;// 第i-1个数
+            f2 = f3;// 第i个数
+        }
+
+        System.out.println(f3);
+    }
+}
